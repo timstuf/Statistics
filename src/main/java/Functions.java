ff1f24db3c9c97a4c33996813be1f1aa6f71a787
timstuf <nastushagrishaeva@gmail.com>
2020-04-01 13:45:11 +0300

    added interval count for expectation using dispersion

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -7,6 +7,7 @@ class Functions {
     private int n;
     private double expectation;
     private double dispersion;
+    private Scanner scanner = new Scanner(System.in);
 
     Functions(int[] values, int[] quantities) {
         this.values = values;
@@ -20,8 +21,8 @@ class Functions {
             expectation += values[i] * quantities[i];
             System.out.printf("%d * %d = %d\n", values[i], quantities[i], values[i] * quantities[i]);
         }
-        System.out.printf("Sum = %f\n", expectation);
-        System.out.printf("Математическое ожидание = %f / %d = %f \n", expectation, n, expectation / n);
+        System.out.printf("Sum = %.2f\n", expectation);
+        System.out.printf("Математическое ожидание = %.2f / %d = %.2f \n", expectation, n, expectation / n);
         System.out.println("____________________________________");
         this.expectation = expectation / n;
     }
@@ -30,26 +31,39 @@ class Functions {
         double dispersion = 0;
         for (int i = 0; i < values.length; i++) {
             dispersion += Math.pow((values[i] - expectation), 2) * quantities[i];
-            System.out.printf("%d * (%d - %.3f)^2 = %f\n", quantities[i], values[i], expectation, Math.pow((values[i] - expectation), 2) * quantities[i]);
+            System.out.printf("%d * (%d - %.2f)^2 = %f\n", quantities[i], values[i], expectation, Math.pow((values[i] - expectation), 2) * quantities[i]);
         }
-        System.out.printf("Выборочная дисперсия = %.3f / %d = %.3f\n", dispersion, n, dispersion / n);
-        System.out.printf("Исправленная выборочная дисперсия = %d/%d * %.3f = %.3f \n", n, n - 1, dispersion / n, dispersion / (n - 1));
+        System.out.printf("Выборочная дисперсия = %.2f / %d = %.2f\n", dispersion, n, dispersion / n);
+        System.out.printf("Исправленная выборочная дисперсия = %d/%d * %.2f = %.2f \n", n, n - 1, dispersion / n, dispersion / (n - 1));
         this.dispersion = dispersion / (n - 1);
         System.out.println("____________________________________");
     }
 
     void intervalExpectation(double alpha) {
-        Scanner scanner = new Scanner(System.in);
         double laplasOfU = (1 - alpha) / 2;
-        System.out.printf("Посмотрите на таблицу Лапласа. Найдите ячейку, значение которой равно %.3f. " +
-                "Сложите строку и колонку, которой соответствует это значение, и введите это число. (Например, если находится на пересечении 1.6 и 0.05 введите 1,65)\n", laplasOfU);
+        System.out.printf("Ф(u/2) = (1 - %.2f) / 2 = %.2f\n", alpha, laplasOfU);
+        System.out.printf("Посмотрите на таблицу Лапласа. Найдите ячейку, значение которой равно %.2f. \n" +
+                "Сложите строку и колонку, которой соответствует это значение, и введите это число. \n(Например, если находится на пересечении 1.6 и 0.05 введите 1,65,\n РАЗДЕЛИТЕЛЬ - ЗАПЯТАЯ)\n", laplasOfU);
         double u2 = scanner.nextDouble();
         double std = Math.sqrt(dispersion);
         double nSqrt = Math.sqrt(n);
         double first = expectation - std/nSqrt*u2;
         double second = expectation + std/nSqrt*u2;
-        System.out.printf("First interval: %.3f - (%.3f * %.3f / %.3f)  =  %.3f\n", expectation, u2, std, nSqrt, first);
-        System.out.printf("First interval: %.3f - (%.3f * %.3f / %.3f)  =  %.3f\n", expectation, u2, std, nSqrt, second);
+        System.out.printf("First bound: %.2f - (%.2f * %.2f / %.2f)  =  %.2f\n", expectation, u2, std, nSqrt, first);
+        System.out.printf("Second bound: %.2f - (%.2f * %.2f / %.2f)  =  %.2f\n", expectation, u2, std, nSqrt, second);
+        System.out.println("____________________________________");
+    }
+
+    void intervalExpectationByDispersion(double alpha){
+        System.out.printf("Посмотрите на таблицу распределения Стьюдента. Число степеней свободы равно %d. \n" +
+                "Уровень значимости равен %.2f.\nВведите число на пересечении, РАЗДЕЛИТЕЛЬ - ЗАПЯТАЯ \n", n-1, alpha);
+        double t = scanner.nextDouble();
+        double std = Math.sqrt(dispersion);
+        double nSqrt = Math.sqrt(n);
+        double first = expectation - std/nSqrt*t;
+        double second = expectation + std/nSqrt*t;
+        System.out.printf("First bound: %.2f - (%.2f * %.2f / %.2f)  =  %.2f\n", expectation, t, std, nSqrt, first);
+        System.out.printf("Second bound: %.2f - (%.2f * %.2f / %.2f)  =  %.2f\n", expectation, t, std, nSqrt, second);
         System.out.println("____________________________________");
     }
 }
